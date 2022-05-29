c9609e3c8d5ce27bfde11b081fdd73c433e753bd
catluynh <catluynh99@gmail.com>
2022-05-29 17:14:12 +0700

    a

--- fe/webpack.config.js
+++ fe/webpack.config.js
@@ -3,5 +3,8 @@ module.exports = {
   devServer: {
     compress: true,
     disableHostCheck: true,
+    port: 3000,
+    contentBase: path.resolve(__dirname, "dist"),
+    historyApiFallback: { index: "/", disableDotRule: true },
   },
 };
