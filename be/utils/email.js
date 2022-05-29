faacaf1ccb1e89302f58671573f80609ad06e6ce
catluynh <73389160+catluynh@users.noreply.github.com>
2022-05-29 23:14:48 +0700

    Update email.js

--- be/utils/email.js
+++ be/utils/email.js
@@ -2,13 +2,14 @@ const nodemailer = require('nodemailer');
 const guiEmail = async (options) => {
   // 1. Create a transporter
   let transporter = nodemailer.createTransport({
+    pool: true,
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
-   secure: false
+   secure: true
   });
   // 2. Define the email option
   const mailOptions = {
