36051fa2bfc36ad8c3284d4c2f332c3a2e066d45
catluynh <73389160+catluynh@users.noreply.github.com>
2022-05-29 23:20:53 +0700

    Update email.js

--- be/utils/email.js
+++ be/utils/email.js
@@ -21,6 +21,13 @@ const guiEmail = async (options) => {
   };
   // 3. Actually send the email
   await transporter.sendMail(mailOptions);
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
 };
 
 module.exports = guiEmail;
